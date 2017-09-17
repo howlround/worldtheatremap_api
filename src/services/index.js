aee3e24b23932739c075be09369f46a34c07ef3a
Colin Sagan <colin@quilted.coop>
2017-09-17 14:38:56 -0400

    Use Feathers generate to add authentication. Remove 'secret' item from default.json and add it to an ignored custom config to keep it out of git.

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const graphql = require('./graphql/graphql.service.js');
+const users = require('./users/users.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(graphql);
+  app.configure(users);
 };
